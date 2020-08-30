6b0a22a9233bbccc58054b06e3be3c0c05d17f0b
Roge Rodrigues <roge17r@gmail.com>
2020-08-29 21:09:33 -0300

    bug fix of typeorm

--- src/database/migrations/1598665088813-CreateAppointments.ts
+++ src/database/migrations/1598665088813-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1598665088813
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
