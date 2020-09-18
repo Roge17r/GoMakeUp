6a47f8476f40f814ad24f810f66e5d63ef15e94d
Roge Rodrigues <roge17r@gmail.com>
2020-09-17 23:18:03 -0300

    identify bug to fix

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-error';
+
 import routes from './routes';
 
 import './database';
+import AppError from './errors/AppError';
 
 const app = express();
 
@@ -10,6 +13,21 @@ app.use(express.json());
 app.use(routes);
 app.get('/', (request, response) => response.json({ message: 'hello world' }));
 
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response
+        .status(err.statusCode)
+        .json({ status: 'error', message: err.message });
+    }
+
+    console.log(err);
+
+    return response
+      .status(500)
+      .json({ status: 'error', message: 'Internal Server Error' });
+  },
+);
 app.listen(3333, () => {
   console.log('🤖 server started on port 3333!');
 });
